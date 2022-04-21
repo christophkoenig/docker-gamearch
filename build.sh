f6bd16c496944611b6e7e6127a6879d59635e5aa
Christoph Koenig <58106705+christophkoenig@users.noreply.github.com>
2022-04-21 17:31:24 +0200

    update version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-VERSION="1.0"
+VERSION="1.0.1"
 IMAGE_NAME="quay.io/ckoenig/gamearch"
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 REPO="https://github.com/Cryptec/GameArch.git"
